e1d0c8bcd9b9063494d802e8dfe44852d94e8403
romboooo <412902@niuitmo.ru>
2026-03-17 19:40:23 +0300

    feat: repositoiries logic for all entities

--- src/main/java/org/example/entity/Review.java
+++ src/main/java/org/example/entity/Review.java
@@ -18,6 +18,10 @@ public class Review {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
---
+++ src/main/java/org/example/repository/FavoriteRepository.java
@@ -0,0 +1,17 @@
+package org.example.repository;
+
+import org.example.entity.Favorite;
+import org.example.entity.Product;
+import org.example.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+    List<Favorite> findByUser(User user);
+    Optional<Favorite> findByUserAndProduct(User user, Product product);
+    void deleteByUserAndProduct(User user, Product product); // из корзины типо
+}
