9543e7f8a02ab12807cbd5e832b7d3700bcf4094
romboooo <romboooo@yandex.ru>
2026-04-30 19:49:23 +0300

    feat: wildfly configuration

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,14 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(Main.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
