37044b514472153cfeda73c3e32b153ad0fb3d9d
romboooo <romboooo@yandex.ru>
2026-05-04 15:11:59 +0300

    fix: jta conf

--- src/main/java/org/example/config/JpaJtaConfig.java
+++ src/main/java/org/example/config/JpaJtaConfig.java
@@ -6,6 +6,7 @@ import org.springframework.jndi.JndiObjectFactoryBean;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
@@ -14,6 +15,7 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
+@EnableTransactionManagement
 public class JpaJtaConfig {
 
     @Bean
@@ -37,6 +39,7 @@ public class JpaJtaConfig {
         jpaProperties.setProperty("hibernate.transaction.jta.platform",
                 "org.hibernate.engine.transaction.jta.platform.internal.JBossAppServerJtaPlatform");
         jpaProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        jpaProperties.setProperty("hibernate.transaction.coordinator_class", "jta");
         em.setJpaProperties(jpaProperties);
         return em;
     }
