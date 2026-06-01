7addcc04af52f221df1f54896ec230acf6728973
mrcdssclss <alexandrevnas@gmail.com>
2026-06-01 21:48:12 +0300

    feat: added Quartz Config + cart cleanup

---
+++ src/main/java/org/example/quartz/QuartzConfig.java
@@ -0,0 +1,34 @@
+package org.example.quartz;
+
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuartzConfig {
+
+    @Bean
+    public JobDetail shoppingCartCleanupJobDetail() {
+        return JobBuilder.newJob(ShoppingCartCleanup.class)
+                .withIdentity("shoppingCartCleanup")
+                .storeDurably()
+                .build();
+    }
+
+    @Bean
+    public Trigger shoppingCartCleanupTrigger(JobDetail shoppingCartCleanupJobDetail) {
+        return TriggerBuilder.newTrigger()
+                .forJob(shoppingCartCleanupJobDetail)
+                .withIdentity("shoppingCartCleanupTrigger")
+                .withSchedule(
+                        SimpleScheduleBuilder.simpleSchedule()
+                                .withIntervalInMinutes(1)
+                                .repeatForever()
+                )
+                .build();
+    }
+}
